5f3c916f3d0f6b69b6983be14d8faeaadb6313d3
tongramt <grantth@tcd.ie>
2020-11-09 14:46:14 +0000

    Code for Lecture 4

---
+++ L-04.R
@@ -0,0 +1,12 @@
+# Title     : Lecture 4
+# Objective : Write and annotate Code for Lecture 2
+# Created by: ThomasGrant
+# Created on: 09/11/2020
+require('fma')
+HoltWinters(dowjones, alpha = 0.5,gamma = FALSE,beta = FALSE)
+# provides you witht the numerical info from the HW algorithm
+plot(forecast(HoltWinters(dowjones, alpha = 0.5,gamma = FALSE,beta = FALSE), n.ahead = 5))
+# Plots the HW algorithm with a certain number of predicions and CI's of 95% and 99%
+HoltWinters(dowjones,gamma = FALSE,beta = FALSE)
+# Automatically uses the alpha which minimises the SSE
+
